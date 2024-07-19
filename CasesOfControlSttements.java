539629b445becd182bc653ff5775ea81310f7a96
DEBASISH PATRA <158206859+DEBASISH077@users.noreply.github.com>
2024-07-19 17:00:56 +0530

    Add files via upload
    JAVA CODES

---
+++ CasesOfControlSttements.java
@@ -0,0 +1,12 @@
+public class CasesOfControlSttements {
+    public static void main(String[] args) {
+        int no1=10;
+        int no2=20;
+        //if single statement then no need of curly braces
+        
+        if(no1>no2)
+        System.out.println(no1);
+        else 
+        System.out.println(no2);
+    }
+}
---
+++ LoopingConditions.java
@@ -0,0 +1,14 @@
+public class LoopingConditions {
+   
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=5;i++){
+            System.out.println("Hello");
+        }
+        int m=2;
+        for(System.out.println("Hi");m<20;m+=2){
+            System.out.println("Hii");
+        }
+
+}
+}
